bfa9086814e75d26eb745f92260c5115e0188c7e
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-28 09:03:55 +0200

    Add solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,37 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('qwerty123456')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Password1!')).toBe(true);
   });
 
   // write more tests here
+
+  it(`should return 'false' for the password
+    with less than 8 characters`, () => {
+    expect(checkPassword('qwerty')).toBe(false);
+  });
+
+  it(`should return 'false' for the password
+    with more than 16 characters`, () => {
+    expect(checkPassword('FullSt@ckDevel0pment')).toBe(false);
+  });
+
+  it(`should return 'false' for the password
+    if it lacks at least 1 digit`, () => {
+    expect(checkPassword('StrongPassword!')).toBe(false);
+  });
+
+  it(`should return 'false' for the password
+    if it lacks at least 1 uppercase letter`, () => {
+    expect(checkPassword('strongp2ssword!')).toBe(false);
+  });
+
+  it(`should return 'false' for the password
+    if it lacks at least 1 special character`, () => {
+    expect(checkPassword('StrongP2ssword')).toBe(false);
+  });
 });
